b8d90e03db52a5f8a10a29facad69be790f60864
Svilen Sultanov <sultanov_s@yahoo.de>
2019-11-09 21:46:35 +0100

    Adding a suggested correction

---
+++ Source/Private/New-SuggestedCorrection.ps1
@@ -0,0 +1,40 @@
+<#
+    .SYNOPSIS
+        Creates a suggested correction
+    .PARAMETER Extent
+        The extent that needs correction
+    .PARAMETER NewString
+        The string that should replace the extent
+    .PARAMETER Description
+        The description that should be shown
+    .OUTPUTS
+        Output (if any)
+#>
+function New-SuggestedCorrection
+{
+    [CmdletBinding()]
+    [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.Language.IScriptExtent]
+        $Extent,
+
+        [Parameter()]
+        [System.String]
+        $NewString,
+
+        [Parameter()]
+        [System.String]
+        $Description
+    )
+
+    [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.CorrectionExtent]::new(
+        $Extent.StartLineNumber,
+        $Extent.EndLineNumber,
+        $Extent.StartColumnNumber,
+        $Extent.EndColumnNumber,
+        $NewString,
+        $Extent.File,
+        $Description
+    )
+}
